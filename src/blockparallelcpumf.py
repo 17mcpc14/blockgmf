f7bfd575d3182bc028d5660e64328224fff9c6d7
17mcpc14 <36224815+17mcpc14@users.noreply.github.com>
2019-04-22 09:54:53 +0530

    Update blockparallelcpumf.py

--- src/blockparallelcpumf.py
+++ src/blockparallelcpumf.py
@@ -1,59 +1,36 @@
 import numpy as np
-import glob
-from os.path import exists, basename
-import sys
 import time
 import math
-import numpy as np
 from util import fetch
 import threading
+from error import rmse
 
-args=list(sys.argv)
-test_input =args[2]
-train_input =args[1]
-blocks=int(args[3])
-steps = int(args[4])
-gpu_steps = int(args[5])
+U,V = [], []
 
-def error(R, P , Q, u1, u2, v1, v2):
-    Rc = np.dot(P,Q)
-    Rc = Rc - R[u1:u2+1,v1:v2+1]
-    Rc = np.square(Rc)
-    error = np.sum(Rc)/(P.shape[0]*Q.shape[1])
-    return np.sqrt(error)
-
-def block_factorization(P, Q, R, u1, u2, v1, v2, steps, K=10, alpha=0.0001, beta=0.01):
+def block_factorization(P, Q, R, u1, u2, v1, v2, steps, K=30, alpha=0.0001, beta=0.01):
     
     t0 = time.clock()
-    steps = int(steps/1)
     print("Steps ",steps)
     if steps<1:
         steps = 1
   
-    Rc = np.zeros((R.shape[0], R.shape[1])) #initPQ(R.shape[0], K, R.shape[1])
-
-    x, y1, y2 = [], [], []
-    
-    flag1 = 1000.0
-    count = 0
     for step in range(steps):
-        #print("Step : " , step)
-        
         for i in range(u2-u1+1):
             for j in range(v2-v1+1):
-                eij = R[i,j] - np.dot(P[i,:], Q[:,j])
-                for k in range(K):
-                    if(np.isfinite(alpha * (2 * eij * Q[k,j] - beta * P[i,k]))):
-                        P[i,k] = P[i,k] + alpha * (2 * eij * Q[k,j] - beta * P[i,k])
-                    if(np.isfinite(alpha * (2 * eij * P[i,k] - beta * Q[k,j]))):
-                        Q[k,j] = Q[k,j] + alpha * (2 * eij * P[i,k] - beta * Q[k,j])
+                if R[i,j] > 0:
+                    eij = R[i,j] - np.dot(P[i,:], Q[:,j])
+                    for k in range(K):
+                        if(np.isfinite(alpha * (2 * eij * Q[k,j] - beta * P[i,k]))):
+                            P[i,k] = P[i,k] + alpha * (2 * eij * Q[k,j] - beta * P[i,k])
+                        if(np.isfinite(alpha * (2 * eij * P[i,k] - beta * Q[k,j]))):
+                            Q[k,j] = Q[k,j] + alpha * (2 * eij * P[i,k] - beta * Q[k,j])
         
     U[u1:u2+1, 0:latent] = P.reshape( (u2-u1+1, latent))
     V[0:latent, v1:v2+1] = Q.reshape( (latent, v2-v1+1))
+    
+def factorize(users, movies, ratings, test_users, test_movies, test_ratings, blocks=1, latent=30, steps=10, gpu_steps=2, alpha=0.00001, beta=0.01, delta=0.01, rmse_repeat_count=3, debug=2, dataset=''):
 
-def factorize(users, movies, ratings, test_users, test_movies, test_ratings, blocks=1, latent=10, steps=10, gpu_steps=2, alpha=0.00001, beta=0.01, delta=0.01, rmse_repeat_count=3, debug=2, dataset=''):
-
-    #U, V = np.ones((R.shape[0], latent)), np.ones((latent, R.shape[1]))
+    U, V = np.ones((np.max(users), latent)), np.ones((latent, np.max(movies)))
     size = max(np.max(users)+1, np.max(movies)+1)
     split = int(size/blocks)
     us = int(math.ceil( np.float(np.max(users))/split ) )
@@ -65,8 +42,8 @@ def factorize(users, movies, ratings, test_users, test_movies, test_ratings, blo
     start_time=time.clock()
     y1, y2 = [], []
     count = 0
-
-    flag1 = error(R, U, V,0, R.shape[1], 0, R.shape[0]) 
+    R = csr_matrix((ratings, (users, movies))).todense()
+    flag1 = rmse(R, U, V,0, R.shape[1], 0, R.shape[0]) 
     
     for k in range(steps):
 
@@ -91,7 +68,8 @@ def factorize(users, movies, ratings, test_users, test_movies, test_ratings, blo
             for j in range(vs):
                 xtemp = int((i+stemp)%us)
                 
-                print("i, j, ii, jj ", i, j, xtemp, j)
+                if debug>1:
+                    print("i, j, ii, jj ", i, j, xtemp, j)
 
                 u1 = xtemp*split
                 if np.max(users) < u1:
@@ -109,7 +87,8 @@ def factorize(users, movies, ratings, test_users, test_movies, test_ratings, blo
                 if np.max(movies) < v2:
                     v2 = int(np.max(movies))
 
-                #print("Processing split : " , i , j, u1, u2, v1, v2)
+                if debug>1:
+                    print("Processing split : " , i , j, u1, u2, v1, v2)
 
                 uu, mm, rr = fetch(u1, u2, v1, v2, users, movies, ratings)
                 if debug>1:
@@ -141,7 +120,7 @@ def factorize(users, movies, ratings, test_users, test_movies, test_ratings, blo
         if debug>1:
             print(" Step time taken : ", round(t5-t4,2))
         y1.append(round(t5-start_time,3))
-        test_rmse = error(R, U, V,0, R.shape[1], 0, R.shape[0]) #e(U, V , test_users, test_movies, test_ratings, min(split, max(np.max(test_users), np.max(test_movies))), latent=latent, debug=debug)
+        test_rmse = rmse(R, U, V,0, R.shape[1], 0, R.shape[0]) #e(U, V , test_users, test_movies, test_ratings, min(split, max(np.max(test_users), np.max(test_movies))), latent=latent, debug=debug)
         print("Step error :", round(test_rmse,3) )
         y2.append(round(test_rmse,3) )
 
@@ -161,20 +140,4 @@ def factorize(users, movies, ratings, test_users, test_movies, test_ratings, blo
         #flag1=flag
 
     np.savetxt(str(blocks*blocks)+'blocks_'+str(gpu_steps)+'iterations_y2.txt', y2, fmt='%.3f')
-    np.savetxt(str(blocks*blocks)+'blocks_'+str(gpu_steps)+'iterations_y1.txt', y1, fmt='%.3f')
-
-R = np.loadtxt('../R.txt')
-users = []
-movies = []
-ratings = []
-for i in range(1000):
-    for j in range(1000):
-        users.append(i)
-        movies.append(j)
-        ratings.append(R[i,j])
-
-latent = 10
-U, V = np.ones((R.shape[0], latent)), np.ones((latent, R.shape[1]))            
-factorize(users, movies, ratings, users, movies, ratings,blocks=blocks, steps=steps, gpu_steps=gpu_steps, debug=1)
-
-
+    np.savetxt(str(blocks*blocks)+'blocks_'+str(gpu_steps)+'iterations_y1.txt', y1, fmt='%.3f')    
