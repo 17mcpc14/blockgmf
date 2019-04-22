81a54e428dada0db3b9db321ee0a92af5f108240
17mcpc14 <36224815+17mcpc14@users.noreply.github.com>
2019-04-22 12:37:13 +0530

    Update cmf.py

--- src/cmf.py
+++ src/cmf.py
@@ -23,15 +23,6 @@ def matrix_factorization(users, movies, ratings, test_users, test_movies, test_r
                     P[i,k] = P[i,k] + alpha * (2 * eij * Q[j,k] - beta * P[i,k])
                     Q[j,k] = Q[j,k] + alpha * (2 * eij * P[i,k] - beta * Q[j,k])
         
-        #e = 0
-        #for idx in range(len(users)):
-        #    if(ratings[idx] > 0):
-        #        i = users[idx]
-        #        j = movies[idx]
-        #        e = e + pow(ratings[idx] - np.dot(P[i,:], Q[j,:].T) , 2)
-        #        for k in range(K):
-        #            e = e + (beta/2) * ( pow(P[i,k],2) + pow(Q[j,k],2) )
-
         print("Time till now :", round(time.clock()-t0,2), "Train error", rmse(users, movies, ratings, P,Q), "Test error", rmse(test_users, test_movies, test_ratings, P,Q) )
 
     return P, Q
