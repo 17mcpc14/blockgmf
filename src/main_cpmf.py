cd04b3a73d3e5b21f72a18b9205520c19b8d27dc
17mcpc14 <36224815+17mcpc14@users.noreply.github.com>
2019-04-22 11:09:56 +0530

    Create main_cpmf.py

---
+++ src/main_cpmf.py
@@ -0,0 +1,21 @@
+import numpy as np
+import sys
+import pandas
+from cpmf import factorize
+
+args=list(sys.argv)
+test_input =args[2]
+train_input =args[1]
+steps = int(args[3])
+blocks = int(args[4])
+
+train = pandas.read_csv(train_input)
+test = pandas.read_csv(test_input)
+users = train['user_id']
+movies = train['movie_id']
+ratings = train['rating']
+test_users = test['user_id']
+test_movies = test['movie_id']
+test_ratings = test['rating']
+
+factorize(users, movies, ratings, test_users, test_movies, test_ratings, K=30, blocks=blocks, steps=steps, alpha=0.0002, beta=0.01, delta=0.01)
