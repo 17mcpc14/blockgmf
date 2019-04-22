4b47b73a4994ed169957b3d83c6d9b418716917b
17mcpc14 <36224815+17mcpc14@users.noreply.github.com>
2019-04-22 11:23:19 +0530

    Update bgmf.py

--- src/bgmf.py
+++ src/bgmf.py
@@ -1,5 +1,7 @@
 import numpy as np
 import time
+from error import rmse
+from util import fetch
 from pycuda import driver, compiler, gpuarray, tools
 import pycuda.autoinit
 
@@ -39,3 +41,133 @@ def matrix_factorization(UU,MM,RR, P, Q, ulimits, plimits, latent=30, gpu_steps=
         print("Shape of P, Q :", P.shape, Q.shape)
 
     return P, Q
+
+def pack(UU, MM, RR, PP, QQ, uu, mm, rr, P, Q, ulimits, plimits):
+
+    ulimits.append(len(uu))
+    plimits.append(P.shape[1]*P.shape[2])
+
+    UU.extend(uu)
+    MM.extend(mm)
+    RR.extend(rr)
+    PP.extend(P.flatten())
+    QQ.extend(Q.flatten())
+
+    return UU, MM, RR, PP, QQ, ulimits, plimits
+
+def unpack(U, V, PP, QQ, users, movies, ulimits, plimits, latent):
+
+    for i in range(len(ulimits)-1):
+        u1 = ulimits[i]
+        u2 = ulimits[i+1]
+        uu = users[u1:u2]
+        P = PP[plimits[i]:plimits[i+1]]
+        P = P.reshape( (len(uu), latent) )
+        U[u1:u2,:]=P
+
+    for i in range(len(ulimits)-1):
+        v1 = ulimits[i]
+        v2 = ulimits[i+1]
+        mm = movies[v1:v2]
+        Q = QQ[plimits[i]:plimits[i+1]]
+        Q = Q.reshape( (len(mm), latent) )
+        V[v1:v2,:]=Q
+
+    return U, V
+
+def factorize(users, movies, ratings, test_users, test_movies, test_ratings, blocks=1, latent=10, steps=10, gpu_steps=2, alpha=0.0002, beta=0.01, delta=0.01, rmse_repeat_count=3, debug=2, dataset=''):
+
+    U, V = np.ones((len(users), latent))*0.1, np.ones((len(movies), latent))*0.1
+    size = max(np.max(users)+1, np.max(movies)+1)
+    split = int(size/blocks)
+    us = int(math.ceil( np.float(np.max(users))/split ) )
+    vs = int(math.ceil( np.float(np.max(movies))/split ) )
+    if debug>1:
+        print("Total splits : ",split, us, vs, us*vs)
+        print("U, V shapes :", U.shape, V.shape)
+
+    start_time=time.clock()
+    y1, y2 = [], []
+    count = 0
+    error = 100 # rmse(users, movies, ratings U, V)
+
+    for k in range(steps):
+
+        if debug>1:
+            print("Step : ", k)
+
+        rmse = 0
+        u1, v1 = 0, 0
+        t4 = time.clock()
+
+        for i in range(us):
+            u1 = i*split
+            if np.max(users) < u1:
+                u1 = int(np.max(users))
+
+            u2 = ((i+1)*split - 1)
+            if np.max(users) < u2:
+                u2 = int(np.max(users))
+
+            stemp = 0
+            UU, MM, RR, PP, QQ = [], [], [], [], []
+            ulimits = [0]
+            plimits = [0]
+
+            for j in range(vs):
+                xtemp = int((i+stemp)%us)
+
+                print("i, j, ii, jj ", i, j, xtemp, j)
+
+                u1 = xtemp*split
+                if np.max(users) < u1:
+                    u1 = int(np.max(users))
+
+                u2 = ((xtemp+1)*split - 1)
+                if np.max(users) < u2:
+                    u2 = int(np.max(users))
+
+                v1 = j*split
+                if np.max(movies) < v1:
+                v2 = (j+1)*split -1
+                if np.max(movies) < v2:
+                    v2 = int(np.max(movies))
+
+                print("Processing split : " , i , j, u1, u2, v1, v2)
+
+                uu, mm, rr = fetch(u1,u2, v1,v2, users,movies,ratings)
+                P, Q = U[[uu],:], V[[mm],:]
+
+                if(len(uu)!=0 and len(mm)!=0):
+                    UU,MM,RR, PP,QQ, ulimits,plimits = pack(UU,MM,RR, PP,QQ, uu,mm,rr, P,Q, ulimits,plimits)
+
+                stemp+=1
+            PP,QQ = matrix_factorization(UU,MM,RR, PP,QQ, ulimits,plimits)
+            U, V = unpack(U,V, PP,QQ, users,movies, ulimits,plimits, latent)
+
+        t5 = time.clock()
+        if debug>1:
+            print(" Step time taken : ", round(t5-t4,2))
+
+        #y1.append(round(t5-start_time,3))
+        train_rmse = rmse(users, movies, ratings, U, V)
+        test_rmse = rmse(test_users, test_movies, test_ratings, U, V)
+        print("Train error:", round(train_rmse, 3) , " Test error:", round(test_rmse,3) )
+        #y2.append(round(test_rmse,3) )
+
+        #step_error=round(test_rmse,4)
+        
+        #if step_error < delta:
+        #    break
+        #elif error<step_error :
+        #    break
+        #elif rmse_repeat_count<count:
+        #    break
+        #elif step_error==error:
+        #    count=count+1
+        #else:
+        #    count = 0
+        #error=step_error
+
+    #np.savetxt(str(blocks*blocks)+'blocks_'+str(gpu_steps)+'iterations_y2.txt', y2, fmt='%.3f')
+    #np.savetxt(str(blocks*blocks)+'blocks_'+str(gpu_steps)+'iterations_y1.txt', y1, fmt='%.3f')
