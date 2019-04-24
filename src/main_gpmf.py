2eb8393e04a92445655d6869a0f63ed5919ff599
17mcpc14 <36224815+17mcpc14@users.noreply.github.com>
2019-04-24 22:39:56 +0530

    Update main_gpmf.py

--- src/main_gpmf.py
+++ src/main_gpmf.py
@@ -6,9 +6,8 @@ from gpmf import factorize
 args=list(sys.argv)
 test_input =args[2]
 train_input =args[1]
-blocks=int(args[3])
-steps = int(args[4])
-gpu_steps = int(args[5])
+steps = int(args[3])
+gpu_steps = int(args[4])
 
 train = pandas.read_csv(train_input).sort_values(by=['user_id','movie_id'])
 test = pandas.read_csv(test_input).sort_values(by=['user_id','movie_id'])
@@ -19,4 +18,4 @@ test_users = test['user_id'].astype(int)
 test_movies = test['movie_id'].astype(int)
 test_ratings = test['rating']
 
-factorize(users, movies, ratings, test_users, test_movies, test_ratings,blocks=blocks, latent=30, steps=steps, gpu_steps=gpu_steps, debug=2)
+factorize(users, movies, ratings, test_users, test_movies, test_ratings, latent=30, steps=steps, gpu_steps=gpu_steps, debug=2)
