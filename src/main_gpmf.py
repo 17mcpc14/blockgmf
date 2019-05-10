742d216c9ed3dec320ed2e1da07b914da15e4a2b
17mcpc14 <36224815+17mcpc14@users.noreply.github.com>
2019-05-10 13:51:16 +0530

    Update main_gpmf.py

--- src/main_gpmf.py
+++ src/main_gpmf.py
@@ -10,11 +10,11 @@ steps = int(args[3])
 
 train = pandas.read_csv(train_input).sort_values(by=['user_id','movie_id'])
 test = pandas.read_csv(test_input).sort_values(by=['user_id','movie_id'])
-users = train['user_id'].astype(int)
-movies = train['movie_id'].astype(int)
+users = train['user_id'].astype(np.int32)
+movies = train['movie_id'].astype(np.int32)
 ratings = train['rating']
-test_users = test['user_id'].astype(int)
-test_movies = test['movie_id'].astype(int)
+test_users = test['user_id'].astype(np.int32)
+test_movies = test['movie_id'].astype(np.int32)
 test_ratings = test['rating']
 
 factorize(users, movies, ratings, test_users, test_movies, test_ratings, latent=30, steps=steps, debug=2)
